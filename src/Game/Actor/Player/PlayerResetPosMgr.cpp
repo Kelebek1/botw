782bc07c99e70d06b68c6440ed5431386740f9d7
Kelebek1 <eeeedddccc@hotmail.co.uk>
2022-02-08 14:05:51 +0000

    Some prog

--- src/Game/Actor/Player/PlayerResetPosMgr.cpp
+++ src/Game/Actor/Player/PlayerResetPosMgr.cpp
@@ -1,4 +1,57 @@
 #include "Game/Actor/Player/PlayerResetPosMgr.h"
+#include "KingSystem/ActorSystem/actBaseProcMgr.h"
+
+SEAD_SINGLETON_DISPOSER_IMPL(Root6)
+
+auto Root6::sub_71009287CC(ksys::act::ActorLinkConstDataAccess* accessor) -> void {
+    if (accessor != nullptr) {
+        accessor->acquire(mSomeActor);
+    }
+}
+
+auto Root6::getCameraActor() -> ksys::act::Actor* {
+    if (mSomeActor && ksys::act::BaseProcMgr::instance() && ksys::act::BaseProcMgr::instance()->isAccessingProcSafe(mSomeActor, nullptr)) {
+        return mSomeActor;
+    }
+    return nullptr;
+}
 
 SEAD_SINGLETON_DISPOSER_IMPL(PlayerResetPosMgr)
 
+auto PlayerResetPosMgr::isNotResetting() -> bool {
+    return mStatus == 0;
+}
+
+auto PlayerResetPosMgr::callCameraM151IfStatus4() -> void {
+    if (mStatus == 4) {
+
+    }
+}
+
+auto PlayerResetPosMgr::addResetPos(const sead::Vector3<f32>& pos, f32 rotation) -> void {
+    mSpinLock.lock();
+    if (mNumResetPos < 64) {
+        mResetPositions[mNumResetPos].setPos(pos);
+        mResetPositions[mNumResetPos].setRot(rotation);
+        mNumResetPos++;
+    }
+    mSpinLock.unlock();
+}
+
+auto PlayerResetPosMgr::setResetPos(const sead::Vector3<f32>& pos, f32 rotation, ksys::act::Actor* actor) -> void {
+    mSpinLock.lock();
+    mResetPosActor = actor;
+    mHasResetPosOverride = true;
+    mResetPosOverride.setPos(pos);
+    mResetPosOverride.setRot(rotation);
+    mSpinLock.unlock();
+}
+
+auto PlayerResetPosMgr::clearResetPos() -> void {
+    mSpinLock.lock();
+    if (mResetPosActor == nullptr) {
+        mHasResetPosOverride = false;
+    }
+    mSpinLock.unlock();
+    mNumResetPos = 0;
+}
--- src/Game/Actor/Player/PlayerResetPosMgr.h
+++ src/Game/Actor/Player/PlayerResetPosMgr.h
@@ -3,10 +3,30 @@
 #include <heap/seadDisposer.h>
 #include <basis/seadTypes.h>
 #include <math/seadVector.h>
+#include <container/seadSafeArray.h>
 #include <thread/seadSpinLock.h>
 #include "KingSystem/ActorSystem/actActor.h"
+#include "KingSystem/ActorSystem/actActorLinkConstDataAccess.h"
+#include "KingSystem/ActorSystem/actCamera.h"
+
+class Root6 {
+    SEAD_SINGLETON_DISPOSER(Root6)
+
+    public:
+        Root6() = default;
+
+        auto sub_71009287CC(ksys::act::ActorLinkConstDataAccess* accessor) -> void;
+        auto getCameraActor() -> ksys::act::Actor*;
+
+        ksys::act::Actor* mSomeActor{};
+};
 
 class ResetPos {
+public:
+    inline auto setPos(const sead::Vector3<f32>& pos_) { pos = pos_; }
+    inline auto setRot(f32 rot) { rotation = rot; }
+
+private:
     sead::Vector3<f32> pos;
     f32 rotation;
 };
@@ -14,25 +34,23 @@ class ResetPos {
 class PlayerResetPosMgr {
     SEAD_SINGLETON_DISPOSER(PlayerResetPosMgr)
 
-    PlayerResetPosMgr() : 
-        numResetPos{},
-        resetPositions{}, 
-        resetPosActor{},
-        hasResetPosOverride{},
-        resetPosOverride{},
-        spinLock{},
-        status{}
-    {
-    };
-
 public:
-    /* 0x020 */ s32 numResetPos;
-    /* 0x024 */ ResetPos resetPositions[64];
-    /* 0x428 */ ksys::act::Actor *resetPosActor;
-    /* 0x430 */ bool hasResetPosOverride;
-    /* 0x434 */ ResetPos resetPosOverride;
-    /* 0x448 */ sead::SpinLock spinLock;
-    /* 0x458 */ int status;
-    /* 0x45C */ sead::Vector3<f32> field_45C;
-    /* 0x468 */ sead::Vector3<f32> field_468;
+    PlayerResetPosMgr() = default;
+
+    auto isNotResetting() -> bool;
+    auto callCameraM151IfStatus4() -> void;
+    auto addResetPos(const sead::Vector3<f32>& pos, f32 rotation) -> void;
+    auto setResetPos(const sead::Vector3<f32>& pos, f32 rotation, ksys::act::Actor* actor) -> void;
+    auto clearResetPos() -> void;
+
+private:
+    s32 mNumResetPos{};
+    sead::SafeArray<ResetPos, 64> mResetPositions{};
+    ksys::act::Actor* mResetPosActor{};
+    bool mHasResetPosOverride{};
+    ResetPos mResetPosOverride{};
+    sead::SpinLock mSpinLock;
+    int mStatus{};
+    sead::Vector3<f32> mUnk_45C;
+    sead::Vector3<f32> mUnk_468;
 };
